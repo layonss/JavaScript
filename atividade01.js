69942c1bdc378eb3bae19bf1264fac3d44397590
layon_rebeldegc@hotmail.com <layon_rebeldegc@hotmail.com>
2022-09-08 13:08:10 -0300

    Atividade1

---
+++ atividade01.js
@@ -0,0 +1,8 @@
+let nome, peso, altura, DI, diabetes //DI = data de internação
+nome = prompt ("\nEscreva seu nome: ") 
+peso = prompt ("\nEscreva seu peso: ") 
+altura = prompt ("\nEscreva sua Altura: ")
+DI = prompt ("\nData de internação: ")
+diabetes = prompt ("\nTem diabetes? ")
+alert ("Seu nome: " + nome + "\nSeu peso é: " + peso + "\nSua altura é: " + altura + "\nData de internação: " + DI + 
+"\nTem diabentes? " + diabetes);
